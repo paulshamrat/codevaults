78d5d85b1fdd8cd30ffbbd36a8bc5918ed4fd83a
Shamrat Kumar Paul <78519466+paulshamrat@users.noreply.github.com>
2023-07-15 00:30:18 +0000

    changes pip

--- 01-installation.py
+++ 01-installation.py
@@ -1,19 +1,19 @@
 # NLP
-! python -m pip install nltk==3.5
-! python -m pip install numpy matplotlib
+python -m pip install nltk==3.5
+python -m pip install numpy matplotlib
 
 # MDAnalysis
-! pip install --upgrade MDAnalysis
-! pip install --upgrade MDAnalysisTests
-! pip install --upgrade MDAnalysisData
+pip install --upgrade MDAnalysis
+pip install --upgrade MDAnalysisTests
+pip install --upgrade MDAnalysisData
 
 # mdtraj, nglview, cython, pytraj, tsplot
 # gnuplot, prody
-! pip install mdtraj
-! pip install nglview
-! pip install cython --upgrade
-! pip install pytraj
-! pip install tsplot
+pip install mdtraj
+pip install nglview
+pip install cython --upgrade
+pip install pytraj
+pip install tsplot
 
 #!sudo apt-get install gnuplot-x11
 #!pip install prody\
